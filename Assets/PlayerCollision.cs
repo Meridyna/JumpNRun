afabba246b0d4476cea19ef1acd0787c5db2eaa4
Meridyna <le.bloemsma@gmail.com>
2022-06-19 16:00:59 +0200

    firstUpload

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
